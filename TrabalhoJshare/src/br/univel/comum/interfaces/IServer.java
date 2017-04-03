3c5ce66735a79fb0e0c11a6acfbdcfb61eeb8f60
Paulo Henrique Bueno <phf.bueno@gmail.com>
2017-04-03 19:55:40 -0300

    Criacao das classes cliente e da interface  IServer

--- TrabalhoJshare/src/br/univel/comum/interfaces/IServer.java
+++ TrabalhoJshare/src/br/univel/comum/interfaces/IServer.java
@@ -1,9 +1,16 @@
 package br.univel.comum.interfaces;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+
+
 
 public interface IServer  extends Remote {
 
 	public static final String NOME_SERVICO = "JShare";
 	
+	public void registrarCliente(Cliente c) throws RemoteException;
+	
+
 }
